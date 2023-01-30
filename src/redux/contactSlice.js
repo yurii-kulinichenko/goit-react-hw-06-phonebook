7d6d96f58c371ef7585b8ef9878ded7e58d93f93
yurakulinichenko <yurii.kulinichenko@intellectsoft.net>
2023-01-30 23:40:37 +0200

    Update contactSlice.js

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -46,7 +46,7 @@ const contactsSlice = createSlice({
       state.isLoading = false;
       state.error = null;
       const index = state.items.findIndex(
-        contact => contact.id === action.payload
+        contact => contact.id === action.payload.id
       );
       state.items.splice(index, 1);
     },
